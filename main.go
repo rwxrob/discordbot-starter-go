e49fc4465fee2da9a4e3eefcbe480407887b07c3
Rob Muhlestein <rob@skilstak.com>
2018-08-13 13:18:18 -0400

    first

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var config Config
+
+type Config struct {
+	Token string `json:"token"`
+}
+
+func main() {
+	loadConfiguration()
+	dg, err := discordgo.New("Bot " + config.Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		os.Exit(1)
+		return
+	}
+	dg.AddHandler(messageCreate)
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		os.Exit(1)
+		return
+	}
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+}
+func loadConfiguration() {
+	jsonFile, err := os.Open("configuration.json")
+	if err != nil {
+		panic(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &config)
+}
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
+}
